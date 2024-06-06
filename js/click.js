2dae1bdc6a521ccdabf2f993250dfef0d4eaaa83
abc01001000 <jiawei4688q@gmail.com>
2024-06-06 09:20:18 +0800

    add click event

--- js/click.js
+++ js/click.js
@@ -2,12 +2,9 @@ document.addEventListener('click', function(e) {
     const ripple = document.createElement('div');
     ripple.className = 'ripple';
     document.body.appendChild(ripple);
-
-    const maxDimension = Math.max(window.innerWidth, window.innerHeight);
-    ripple.style.width = ripple.style.height = `${maxDimension}px`;
-
-    ripple.style.left = `${e.clientX - maxDimension / 2}px`;
-    ripple.style.top = `${e.clientY - maxDimension / 2}px`;
+    ripple.style.width = ripple.style.height = `50px`;
+    ripple.style.left = `${e.clientX + window.scrollX - 25}px`;
+    ripple.style.top = `${e.clientY + window.scrollY - 25}px`;
     console.log(`(x, y) = (${e.clientX}, ${e.clientY}`)
 
     ripple.addEventListener('animationend', () => {
